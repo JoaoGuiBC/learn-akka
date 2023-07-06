0919f7692165d30b7c388e0ee6d9796823cfab0a
João Guilherme Da Rocha <joaoguibc@gmail.com>
2023-07-06 17:32:28 +0000

    Actors

--- src/main/scala/actors/ActorCapabilities.scala
+++ src/main/scala/actors/ActorCapabilities.scala
@@ -59,7 +59,7 @@ object ActorCapabilities extends App {
     4 - dead letters
    */
 
-  jane ! "Hi!" // reply to "me"
+  // jane ! "Hi!" // reply to "me"
 
   /*
     5 - forwarding messages
@@ -70,4 +70,76 @@ object ActorCapabilities extends App {
 
   case class WirelessPhoneMessage(content: String, ref: ActorRef)
   jane ! WirelessPhoneMessage("Hi", john) // no sender
+
+  // DOMAIN of the counter
+  object Counter:
+    case object Increment
+    case object Decrement
+    case object Print
+
+  class Counter extends Actor:
+    import Counter._
+
+    var value = 0
+    override def receive: Actor.Receive =
+      case Increment => value += 1
+      case Decrement => value -= 1
+      case Print     => println(s"[${self.path}]: O valor atual é: $value")
+
+  val counter = system.actorOf(Props(Counter()), "counter")
+
+  counter ! Counter.Print
+  counter ! Counter.Increment
+  counter ! Counter.Increment
+  counter ! Counter.Decrement
+  counter ! Counter.Print
+
+  object Bank:
+    case class Deposit(value: Int)
+    case class Withdraw(value: Int)
+    case object Statement
+
+    case class TransactionSuccess(message: String)
+    case class TransactionFailure(message: String)
+
+  class Bank extends Actor:
+    import Bank._
+
+    var amount = 0
+    override def receive: Actor.Receive =
+      case Deposit(value) =>
+        if amount < 0 then
+          sender() ! TransactionFailure("Valor inválido para depósito")
+        else
+          amount += value
+          sender() ! TransactionSuccess("Depósito realizado com sucesso")
+      case Withdraw(value) =>
+        if amount < 0 then
+          sender() ! TransactionFailure("Valor inválido para saque")
+        else if amount - value < 0 then
+          sender() ! TransactionFailure("Fundos insuficientes para saque")
+        else
+          amount -= value
+          sender() ! TransactionSuccess("Saque realizado com sucesso")
+      case Statement => sender() ! s"O seu saldo é: $amount"
+
+  object Person:
+    case class LiveTheLife(account: ActorRef)
+
+  class Person extends Actor:
+    import Person._
+    import Bank._
+
+    override def receive: Actor.Receive =
+      case LiveTheLife(account) =>
+        account ! Deposit(1000)
+        account ! Withdraw(2000)
+        account ! Withdraw(500)
+        account ! Statement
+      case message => println(message.toString)
+
+  val account = system.actorOf(Props(Bank()), "account")
+  val person = system.actorOf(Props(Person()), "person")
+
+  person ! Person.LiveTheLife(account)
 }
